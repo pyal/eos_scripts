93f566e4abb0c63ceb6bef24cce4ba7c976208c8
U-CHAMBER\ternovoi <ternovoi@ficp.ac.ru>
2016-08-03 10:27:35 +0200

    bug fix

--- scripts/gasd/gasd.mk
+++ scripts/gasd/gasd.mk
@@ -23,12 +23,12 @@ CheckCfg.dst :
 	$(call CheckCommand,BASE)
 	$(call CheckCommand,CFGFILE)
 
-getmatter.dst : 
+getmatter.dst :
 	mkdir -p $(WorkDir) || echo wau
 	cd $(WorkDir);make -f $(ScriptDir)/mat.mk getmatter.dst
 	$(markstep)
 
-getscript.dst : CheckCfg.dst getmatter.dst 
+getscript.dst : CheckCfg.dst getmatter.dst
 	cp $(ScriptDir)/* $(WorkDir) || echo wau
 	cp $$CFGFILE $(WorkDir) || echo wau
 	#$(markstep)
@@ -45,14 +45,14 @@ run1.dst : getscript.dst CheckCfg.dst
 
 
 $(MkPref).files := scripts/gasd/$(MkPref).mk scripts/gasd/$(MkPref).pl scripts/gasd/$(MkPref).cfg scripts/gasd/GasdCfgReader.pm
-$(MkPref).files := $($(MkPref).files) scripts/gasd/lib/AnalyzeData.pm scripts/gasd/lib/ClcGasd.pm scripts/gasd/lib/RegMarch.pm 
+$(MkPref).files := $($(MkPref).files) scripts/gasd/lib/AnalyzeData.pm scripts/gasd/lib/ClcGasd.pm scripts/gasd/lib/RegMarch.pm
 $(MkPref).files := $($(MkPref).files) scripts/common/mat.mk scripts/common/cinc.mk
 
 updatescript.dst :
 	rm -rf $(gitdir) || echo cannot remove
-	mkdir -p $(MatterDir) || echo cannot make dir 
-	mkdir -p $(WorkDir) || echo cannot make dir 
-	git clone $$GITROOT/$(gitdir) $(gitdir) > /dev/null
+	mkdir -p $(MatterDir) || echo cannot make dir
+	mkdir -p $(WorkDir) || echo cannot make dir
+	git clone $(GITROOT) $(gitdir) > /dev/null
 	for aa in $($(MkPref).files) ; do \
 		bb=$$(basename $$aa) ;\
 		cp $(gitdir)/$$aa $(ScriptDir)/$$bb ;\
@@ -62,7 +62,7 @@ updatescript.dst :
 
 exportscript.dst :
 	rm -rf $(gitdir) && mkdir -p $(gitdir)
-	git clone $$GITROOT/$(gitdir) $(gitdir) > /dev/null
+	git clone $(GITROOT) $(gitdir) > /dev/null
 	for aa in $($(MkPref).files) ; do \
 		bb=$$(basename $$aa) ;\
 		cp $(ScriptDir)/$$bb $(gitdir)/$$aa  ;\
