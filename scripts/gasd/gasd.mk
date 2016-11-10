25f4ff4ac73944bf1528ba0efa40034ffe05b2dc
pyal <apyalling@gmail.com>
2016-11-10 09:21:12 -0800

    adding brass mg matters to gasd clc

--- scripts/gasd/gasd.mk
+++ scripts/gasd/gasd.mk
@@ -43,6 +43,10 @@ run1.dst : getscript.dst CheckCfg.dst
 		perl $(ScriptDir)/$(MkPref).pl $(WorkDir) $$CFGFILE $(WorkDir)/matter_name.txt $$BASE $$aa 1e3;\
 		done
 
+bug.dst :  CheckCfg.dst
+	cd $(WorkDir);for aa in GetAllPnt SumFiles ; do \
+		perl $(ScriptDir)/$(MkPref).pl $(WorkDir) $$CFGFILE $(WorkDir)/matter_name.txt $$BASE $$aa 1e8;\
+		done
 
 $(MkPref).files := scripts/gasd/$(MkPref).mk scripts/gasd/$(MkPref).pl scripts/gasd/$(MkPref).cfg scripts/gasd/GasdCfgReader.pm
 $(MkPref).files := $($(MkPref).files) scripts/gasd/lib/AnalyzeData.pm scripts/gasd/lib/ClcGasd.pm scripts/gasd/lib/RegMarch.pm
